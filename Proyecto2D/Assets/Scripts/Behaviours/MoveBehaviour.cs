de04057b5735fba733d2a3afbc0e7d42add711f7
Laia <a17lairomfra@inspedralbes.cat>
2020-02-17 20:53:03 +0100

    Actualizacion

---
+++ Proyecto2D/Assets/Scripts/Behaviours/MoveBehaviour.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class MoveBehaviour : MonoBehaviour
+{
+    [SerializeField]
+    private Vector2 _direction;
+    [SerializeField]
+    private float _speed;
+
+    private Rigidbody2D _rb;
+
+    public void Awake()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+    }
+    public void move()
+    {
+        _rb.MovePosition(new Vector2(transform.position.x + _speed * Time.deltaTime * _direction.x , transform.position.y + _speed * Time.deltaTime * _direction.y));
+    }
+    public void move(Vector2 NewDirection)
+    {
+        _rb.MovePosition(new Vector2(transform.position.x + _speed * Time.deltaTime * NewDirection.x , transform.position.y + _speed * Time.deltaTime * NewDirection.y));
+    }
+}
---
+++ Proyecto2D/Assets/Scripts/InGameObjects/Player.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private MoveBehaviour _movebehaviour;
+    private Vector2 _direction;
+    private Animator _animation;
+    public void Awake()
+    {
+        _animation = GetComponent<Animator>();
+        _movebehaviour = GetComponent<MoveBehaviour>();
+    }
+
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.W))
+        {
+            _animation.SetInteger("State", 0);
+            _movebehaviour.move(Vector2.up);
+        }
+        if(Input.GetKey(KeyCode.S))
+        {
+            _animation.SetInteger("State", 1);
+            _movebehaviour.move(Vector2.down);
+        }
+        if(Input.GetKey(KeyCode.A))
+        {
+            _animation.SetInteger("State", 2);
+            _movebehaviour.move(Vector2.left);
+        }
+        if(Input.GetKey(KeyCode.D))
+        {
+            _animation.SetInteger("State", 2);
+            _movebehaviour.move(Vector2.right);
+        }
+        if(!Input.anyKey)
+        {
+            _animation.SetInteger("State", 0); 
+        }
+    }
+}
